fbeefff40e843b60f0ee7cf8fd6571b22fcbd320
Azamat <63450583+azamat-aminov@users.noreply.github.com>
2023-08-06 14:18:27 +0500

    Time: 5 ms (33.09%), Space: 41.5 MB (33.49%) - LeetHub

---
+++ 1816-truncate-sentence/1816-truncate-sentence.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String truncateSentence(String s, int k) {
+        String[] words = s.split(" ");
+        String outPut = "";
+        for(int i = 0; i < k; i++ ){
+           outPut = outPut + words[i] + " ";
+        }
+        return outPut.substring(0, outPut.length() - 1);
+    }
+}
