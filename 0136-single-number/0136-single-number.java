8ab70b9795766eddaed43fba817c312488b47fab
Azamat <63450583+azamat-aminov@users.noreply.github.com>
2023-08-08 01:25:52 +0500

    Time: 10 ms (28.15%), Space: 44 MB (89.09%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,12 +1,16 @@
 class Solution {
     public int singleNumber(int[] nums) {
         Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++){
-           if (map.get(nums[i]) == null){
-               map.put(nums[i], 1);
-           }else{
-             map.put(nums[i], map.get(nums[i]) + 1);  
-           }     
+        // for (int i = 0; i < nums.length; i++){
+        //    if (map.get(nums[i]) == null){
+        //        map.put(nums[i], 1);
+        //    }else{
+        //      map.put(nums[i], map.get(nums[i]) + 1);  
+        //    }     
+        // }
+        
+        for(int num : nums){
+            map.merge(num, 1, Integer::sum);
         }
         
         for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
