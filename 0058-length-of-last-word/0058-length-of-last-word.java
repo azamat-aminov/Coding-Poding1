0a453fc5faae5bb595065c271237700f67ab369c
Azamat <63450583+azamat-aminov@users.noreply.github.com>
2023-08-08 22:02:39 +0500

    Time: 0 ms (100.00%), Space: 40.6 MB (89.34%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,6 +1,20 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-       String[] words =  s.split(" ");
-      return words[words.length - 1].length();
+        
+    // String[] words =  s.split(" ");
+    // return words[words.length - 1].length();
+    
+    int lengthOfTheWord = 0;
+        
+    for(int i = s.length() - 1; i >= 0; i--){
+        if (s.charAt(i) != ' '){
+            lengthOfTheWord = lengthOfTheWord + 1;
+        }else{
+            if(lengthOfTheWord > 0){
+                return lengthOfTheWord;
+            }
+        }
     }
+    return lengthOfTheWord;
+    }  
 }
