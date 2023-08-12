75ef3a7f2623ba1c4ba67cc83dfe8bbacbb692c2
Azamat <63450583+azamat-aminov@users.noreply.github.com>
2023-08-12 18:10:34 +0500

    Time: 4 ms (22.76%), Space: 40.4 MB (77.58%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.java
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.java
@@ -1,17 +1,19 @@
 class Solution {
   public String sortSentence(String s) {
 
-    Map<Integer, String> map = new HashMap<>();
+    // Map<Integer, String> map = new HashMap<>();
+     
     String[] words = s.split(" ");
+    String [] elements = new String[words.length + 1];
     for (int i = 0; i < words.length; i++) {
       char index = words[i].charAt(words[i].length() - 1);
-      map.put(Integer.parseInt(String.valueOf(index)), words[i].substring(0, words[i].length() - 1));
+      elements[Integer.parseInt(String.valueOf(index))] = words[i].substring(0, words[i].length() - 1);
     }
-
-    String fullSentence = "";
-    for (Map.Entry<Integer, String> entry : map.entrySet()) {
-      fullSentence = fullSentence + entry.getValue() + " ";
+    String full = "";
+    for (int i = 1; i < elements.length; i++) {
+    full = full + elements[i] + " ";
     }
-    return fullSentence.substring(0, fullSentence.length() - 1);
-  }
+      return full.substring(0, full.length() - 1);
+    }
 }
+  
