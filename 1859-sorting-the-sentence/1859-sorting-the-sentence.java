af8fc80dfb91be1cb9f25ad6cfbcb1a47970e35f
Azamat <63450583+azamat-aminov@users.noreply.github.com>
2023-08-12 19:04:15 +0500

    Time: 0 ms (100.00%), Space: 41 MB (8.12%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.java
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.java
@@ -1,13 +1,11 @@
 class Solution {
   public String sortSentence(String s) {
-
-    // Map<Integer, String> map = new HashMap<>();
      
     String[] words = s.split(" ");
-    String [] elements = new String[words.length + 1];
+    String[] elements = new String[words.length + 1];
     for (int i = 0; i < words.length; i++) {
-      char index = words[i].charAt(words[i].length() - 1);
-      elements[Integer.parseInt(String.valueOf(index))] = words[i].substring(0, words[i].length() - 1);
+      String index = words[i].substring(words[i].length() - 1);
+      elements[Integer.parseInt(index)] = words[i].substring(0, words[i].length() - 1);
     }
     StringBuilder builder = new StringBuilder();
     for (int i = 1; i < elements.length ; i++) {
