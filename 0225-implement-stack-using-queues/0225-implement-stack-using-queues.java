91604402a720a6e8ccb12f3e0c68ba58a018e05f
Azamat <63450583+azamat-aminov@users.noreply.github.com>
2023-08-22 00:04:22 +0500

    Time: 0 ms (100.00%), Space: 40.1 MB (80.13%) - LeetHub

---
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
@@ -0,0 +1,36 @@
+class MyStack {
+    
+    int [] a;
+    int N;
+
+    public MyStack() {
+        a = new int[100];
+    }
+    
+    public void push(int x) {
+        a[N++] = x;
+    }
+    
+    public int pop() {
+        int item = a[--N];
+        // a[N] = null;
+        return item;
+    }
+    
+    public int top() {
+        return a[N - 1];
+    }
+    
+    public boolean empty() {
+        return N == 0;
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
