2a3f1cd771a1657b6abfbfb33963bdcb9b1387e7
Azamat <63450583+azamat-aminov@users.noreply.github.com>
2023-08-23 23:55:44 +0500

    Time: 2 ms (64.93%), Space: 41 MB (22.98%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.java
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -1,45 +1,43 @@
 class Solution {
     public boolean backspaceCompare(String s, String t) {
-        
+
         Stack<Character> firstStack = new Stack<>();
         Stack<Character> secondStack = new Stack<>();
-        
+
         char[] first = s.toCharArray();
         char[] second = t.toCharArray();
-        
-        for(int i = 0; i < first.length; i++){
-            if(first[i] == '#'){
-                if(!firstStack.isEmpty()){
+
+        for (int i = 0; i < first.length; i++) {
+            if (first[i] == '#') {
+                if (!firstStack.isEmpty()) {
                     firstStack.pop();
                 }
-            }else{
-                firstStack.push(first[i]);   
+            } else {
+                firstStack.push(first[i]);
             }
         }
-        for(int k = 0; k < second.length; k++){
-            if(second[k] == '#'){
-                if(!secondStack.isEmpty()){
+        for (int k = 0; k < second.length; k++) {
+            if (second[k] == '#') {
+                if (!secondStack.isEmpty()) {
                     secondStack.pop();
                 }
-            }else{
-                secondStack.push(second[k]);   
+            } else {
+                secondStack.push(second[k]);
             }
         }
-        
+
 //         StringBuilder sFirst = new StringBuilder();
-        
 //         while(!firstStack.isEmpty()){
 //             // sFirst = sFirst + String.valueOf(firstStack.pop());
 //             sFirst.append(String.valueOf(firstStack.pop()));
 //         }
-        
-//          StringBuilder tSecond = new StringBuilder();
-        
+//          StringBuilder tSecond = new StringBuilder(); 
 //         while(!secondStack.isEmpty()){
 //             // tSecond = tSecond + String.valueOf(secondStack.pop());
 //             tSecond.append(String.valueOf(secondStack.pop()));
 //         }
-        return firstStack.equals(secondStack);
         // return sFirst.toString().equals(tSecond.toString());
+
+        return firstStack.equals(secondStack);
     }
 }
