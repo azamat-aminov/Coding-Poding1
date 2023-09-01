83015f8cec905782177eb5d7450d7c1295a650bb
Azamat <63450583+azamat-aminov@users.noreply.github.com>
2023-09-01 06:06:24 +0500

    Time: 2 ms (73.73%), Space: 46.3 MB (66.16%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -2,17 +2,11 @@ public class Solution {
     public int[] countBits(int num) {
         
         int[] result = new int[num + 1];
-        for (int i = 0; i <= num; i++) {  
-            int number = i;
-            int bits = 0;
-            while (number > 0) {
-                if ((number & 1) == 1) {
-                    bits++;
-                }
-                number = number >> 1;
-            }
-            result[i] = bits;
+         
+        for (int i = 1; i < result.length; i++) {
+            result[i] = result[i & (i - 1)] + 1;
         }
+         
         return result;
     }
 }
