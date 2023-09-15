e1b66770973a8c57e6b1cda1573402721f028bcd
Azamat <63450583+azamat-aminov@users.noreply.github.com>
2023-09-15 14:53:45 +0500

    Time: 9 ms (5.99%), Space: 42.2 MB (87.17%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -1,27 +1,37 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
         
+    Set<Integer> set = new HashSet<>();
         
-        Set<Integer> sets1 = new HashSet<>();
-        
-        Set<Integer> sets2 = new HashSet<>();
-        
-        List<Integer> list = new ArrayList<>();
-        
-        for (int i = 0; i < nums1.length ; i++) {
-            sets1.add(nums1[i]);
-        }
-        
-        for (int i = 0; i <nums2.length; i++) {
-            sets2.add(nums2[i]);
-        }
-        
-
-        for (Integer i : sets1) {
-            if (sets2.contains(i)) {
-                list.add(i);
+    for (int i = 0; i < nums1.length ; i++) {
+            for (int j = 0; j < nums2.length; j++) {
+                if(nums1[i] == nums2[j]){
+                    set.add(nums2[j]);
+                }
             }
         }
-        return list.stream().mapToInt(i -> i).toArray();
+        
+      return set.stream().mapToInt(Integer::intValue).toArray();
+    
+        
+//         Set<Integer> sets2 = new HashSet<>();
+        
+//         List<Integer> list = new ArrayList<>();
+        
+//         for (int i = 0; i < nums1.length ; i++) {
+//             sets1.add(nums1[i]);
+//         }
+        
+//         for (int i = 0; i < nums2.length; i++) {
+//             sets2.add(nums2[i]);
+//         }
+        
+
+//         for (Integer i : sets1) {
+//             if (sets2.contains(i)) {
+//                 list.add(i);
+//             }
+//         }
+//         return list.stream().mapToInt(i -> i).toArray();
     }
 }
