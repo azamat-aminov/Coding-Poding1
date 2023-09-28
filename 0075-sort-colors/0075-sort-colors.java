e320baf359d56e9454832275ce4deeed2b24f3f5
Azamat <63450583+azamat-aminov@users.noreply.github.com>
2023-09-29 01:59:48 +0500

    Time: 5 ms (6.26%), Space: 41 MB (31.90%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,14 +1,34 @@
 class Solution {
     public void sortColors(int[] nums) {
         
-        for(int i = 0; i < nums.length; i++){
-            for(int j = i+1; j < nums.length; j++){
-                if(nums[i] > nums[j]){
-                    int temp = nums[i];
-                    nums[i] = nums[j];
-                    nums[j] = temp;
+        // for(int i = 0; i < nums.length; i++){
+        //     for(int j = i+1; j < nums.length; j++){
+        //         if(nums[i] > nums[j]){
+        //             int temp = nums[i];
+        //             nums[i] = nums[j];
+        //             nums[j] = temp;
+        //         }
+        //     }
+        // }
+        int N = nums.length;
+        for (int i = 0; i < N - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < N; j++) {
+                if (less(nums[j], nums[min])) {
+                    min = j;
                 }
             }
-        }      
+                exchange(nums, i, min);
+        }
+    }
+    
+    private boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+    
+     private void exchange(int[] a, int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
     }
 }
