77b701556bfe2442473bf078f8dcd47e7853bc87
Azamat <63450583+azamat-aminov@users.noreply.github.com>
2023-09-29 23:54:07 +0500

    Time: 9 ms (59.74%), Space: 43.6 MB (58.04%) - LeetHub

--- 2733-neither-minimum-nor-maximum/2733-neither-minimum-nor-maximum.java
+++ 2733-neither-minimum-nor-maximum/2733-neither-minimum-nor-maximum.java
@@ -7,25 +7,29 @@ class Solution {
             return -1;
         }
         
-        int max = nums[0];
-        int min = nums[0];
+        Arrays.sort(nums);
         
-        for(int i = 0; i < N; i++){
-            if(nums[i] > max){
-                max = nums[i];
-            }
-            if(nums[i] < min){
-                min = nums[i];
-            }
-        }
+        return nums[1];
         
-        int result = 0;
+//         int max = nums[0];
+//         int min = nums[0];
         
-        for(int j = 0; j < N; j++){
-            if(nums[j] != max && nums[j] != min){
-                result = nums[j];
-            }
-        }
-        return result;
+//         for(int i = 0; i < N; i++){
+//             if(nums[i] > max){
+//                 max = nums[i];
+//             }
+//             if(nums[i] < min){
+//                 min = nums[i];
+//             }
+//         }
+        
+//         int result = 0;
+        
+//         for(int j = 0; j < N; j++){
+//             if(nums[j] != max && nums[j] != min){
+//                 result = nums[j];
+//             }
+//         }
+//         return result;
     }
 }
