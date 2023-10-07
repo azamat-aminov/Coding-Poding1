15b9aab4a5ea3f3e23e8417bf3340203d8efa064
Azamat <63450583+azamat-aminov@users.noreply.github.com>
2023-10-07 16:51:02 +0500

    Time: 2 ms (96.15%), Space: 40.1 MB (15.12%) - LeetHub

--- 2652-sum-multiples/2652-sum-multiples.java
+++ 2652-sum-multiples/2652-sum-multiples.java
@@ -5,7 +5,7 @@ class Solution {
         
         for(int i = 1; i <= n; i++){
             if(i % 3 == 0 || i % 5 == 0 || i % 7 == 0){
-                sum = sum + i;           
+                sum += i;           
             }
         }
         
