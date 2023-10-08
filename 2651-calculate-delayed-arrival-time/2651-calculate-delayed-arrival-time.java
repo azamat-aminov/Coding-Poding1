86748664a1fe76812a4879ce375442ed3dababff
Azamat <63450583+azamat-aminov@users.noreply.github.com>
2023-10-08 22:57:07 +0500

    Time: 0 ms (100.00%), Space: 40 MB (5.37%) - LeetHub

--- 2651-calculate-delayed-arrival-time/2651-calculate-delayed-arrival-time.java
+++ 2651-calculate-delayed-arrival-time/2651-calculate-delayed-arrival-time.java
@@ -1,7 +1,7 @@
 class Solution {
     public int findDelayedArrivalTime(int arrivalTime, int delayedTime) {  
         
-//         int actualTime = arrivalTime + delayedTime;
+        int actualTime = arrivalTime + delayedTime;
         
 //         if(actualTime == 24) {
 //             actualTime = 0;
@@ -9,7 +9,7 @@ class Solution {
 //             actualTime = actualTime - 24;
 //         }
         
-      return arrivalTime + delayedTime == 24 ? 0 : (arrivalTime + delayedTime > 24 ? arrivalTime + delayedTime - 24 : arrivalTime + delayedTime);
+      return actualTime == 24 ? 0 : (actualTime > 24 ? actualTime - 24 : actualTime);
         
         // return actualTime;
     }
