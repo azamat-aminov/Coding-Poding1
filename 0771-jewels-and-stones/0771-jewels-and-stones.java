658432834fed00b3a1ab2327a4096608ee09aa29
Azamat <63450583+azamat-aminov@users.noreply.github.com>
2023-10-15 12:04:09 +0500

    Time: 2 ms (11.00%), Space: 41 MB (15.49%) - LeetHub

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        
+        Map<Character, Integer> map = new HashMap<>();
+        
+        char[] chars = jewels.toCharArray();
+        
+        for(char ch : stones.toCharArray()) {
+            map.merge(ch, 1, Integer::sum);
+        }
+        
+        int sum = 0;
+        
+        for(char ch : chars) {
+           if(map.get(ch) != null){
+               sum = sum + map.get(ch);
+           }
+        }
+        
+        return sum;
+    }
+}
