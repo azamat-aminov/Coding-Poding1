08f51a103b91b653bca58e6f4a68ddc88cfb5ac5
Azamat <63450583+azamat-aminov@users.noreply.github.com>
2023-10-17 10:38:59 +0500

    Time: 2 ms (98.30%), Space: 40.4 MB (99.81%) - LeetHub

--- 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.java
+++ 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.java
@@ -4,9 +4,11 @@ class Solution {
         int counter = 0;
         
         for(int i = 0; i < nums.size(); i++) {
+
             for(int j = i + 1; j < nums.size(); j++) {
                 if(nums.get(i) + nums.get(j) < target) {
                     counter++;
+                    // System.out.println(i +" " + j);
                 }
             }
         }
