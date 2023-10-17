0e5bcbb788dc73dfb7cbe0bf17e2e898cbe52b30
Azamat <63450583+azamat-aminov@users.noreply.github.com>
2023-10-17 22:30:44 +0500

    Time: 1 ms (100.00%), Space: 45.1 MB (75.81%) - LeetHub

--- 1720-decode-xored-array/1720-decode-xored-array.java
+++ 1720-decode-xored-array/1720-decode-xored-array.java
@@ -1,9 +1,6 @@
 class Solution {
     public int[] decode(int[] encoded, int first) {
         
-        // encoded[i] = arr[i] + arr[i + 1];
-        // arr[0] = 1; 1 - 1 arr[1] = 0;
-        
         int[] result = new int[encoded.length + 1];
         result[0] = first;
         for(int i = 0; i < encoded.length; i++) {
