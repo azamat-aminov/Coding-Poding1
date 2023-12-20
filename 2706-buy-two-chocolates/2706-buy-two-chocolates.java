9b43aa7eff7c744adcdaebafce4d0b1da686d1bc
Azamat <63450583+azamat-aminov@users.noreply.github.com>
2023-12-20 23:09:26 +0500

    Time: 1 ms (100.00%), Space: 44.2 MB (5.53%) - LeetHub

---
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int buyChoco(int[] prices, int money) {
+        
+        int min = Integer.MAX_VALUE;;
+        
+        for(int i = 0; i < prices.length; i++) {
+            for(int j = 0; j < prices.length; j++) {
+                if(i != j) {
+                    int sum = prices[i] + prices[j];
+                    min = Math.min(sum, min);
+                }
+            }
+        }
+        
+        int result = money - min;
+        
+        if(result < 0) {
+            return money;
+        } 
+        
+        return result;
+    }
+}
